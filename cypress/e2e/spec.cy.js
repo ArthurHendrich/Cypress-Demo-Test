1db6718d720f55f6ef7537db5169cee6049081d7
Hendrich <arthuralenc@gmail.com>
2022-08-11 20:09:38 -0300

    First commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,41 @@
+describe("verify an test in saucemo", () => {
+  beforeEach(() => {
+    cy.visit("https://www.saucedemo.com/");
+  });
+
+  it("try to log in", () => {
+    cy.get("#user-name").type("standard_user");
+    cy.get("#password").type("secret_sauce");
+    cy.get("#login-button").click();
+
+    // verify if is in page
+    cy.get("#inventory_container").should("be.visible");
+  });
+
+  context("If is logged in", () => {
+    beforeEach(() => {
+      cy.get("#user-name").type("standard_user");
+      cy.get("#password").type("secret_sauce");
+      cy.get("#login-button").click();
+
+      // verify if is in page
+      cy.get("#inventory_container").should("be.visible");
+    });
+
+    it("try to add a product to cart", () => {
+      const productName = "Sauce Labs Backpack";
+
+      // find the product and click on it
+      cy.contains(productName);
+      cy.get("#add-to-cart-sauce-labs-backpack").click();
+
+      // verify if name Add to Cart change to Remove from Cart
+      cy.get("#remove-sauce-labs-backpack").should("have.text", "Remove");
+
+      // verify if product was added
+      cy.get("#shopping_cart_container").should("be.visible");
+      // or cy.get("#cart-status > a").should("have.text", "1");
+    });
+    
+  });
+});
